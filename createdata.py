b2b7734864ed1711588418996b07adc08facb80c
Gary <comusus@gmail.com>
2018-04-25 03:52:27 -0400

    Creates Json file for each of top 50 cities and each essay with the top words and counts but there's probably a better way to format this

---
+++ createdata.py
@@ -0,0 +1,38 @@
+import pandas as pd
+import numpy as np
+from bs4 import BeautifulSoup
+from sklearn.feature_extraction.text import CountVectorizer
+import operator
+import json
+
+
+with open("profiles.csv") as f:
+    df = pd.read_csv(f)
+
+
+N = 1
+top_n = df.location.value_counts().head(n=N).index.tolist() #np array
+essays = ["essay0", "essay1", "essay2", "essay3", "essay4", "essay5", "essay6", "essay7", "essay8", "essay9"]
+
+def preprocessed(s):
+    return BeautifulSoup(s, "html.parser").get_text()
+
+with open('okcupid.json', 'w') as outfile:
+
+	for i in range(N): #N is top n cities
+		essay_dict = {}
+		for essay in essays:
+			cvect = CountVectorizer(stop_words="english", preprocessor = preprocessed, min_df=10, max_df = 0.95, max_features =200, ngram_range=(1,2))
+			counts = cvect.fit_transform(df[df.location == top_n[i]][essay].values.astype('U'))
+			counts_sum = (counts.toarray().sum(axis=0)).tolist()
+			cvect_dict = cvect.vocabulary_ #ohh these are just indexes cuz vector is always the same cuz vector space model
+			# newcvect_dict = {}
+			word = []
+			word_count = []
+			for key, value in cvect_dict.items():
+				word.append(key)
+				word_count.append(value)
+			# 	newcvect_dict[key] = counts_sum[value]
+
+			# sorted_top = sorted(newcvect_dict.items(), key=operator.itemgetter(1), reverse=True)
+			json.dump({'city':top_n[i], 'essay':essay, 'top_word':word, 'top_count':word_count}, outfile, indent=4)
